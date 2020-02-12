446b355dd8fbfb3519018e3a1f8fa196d0ebf017
Антон Поляков <polyakov9.anton9@gmail.com>
2020-02-12 19:29:33 +0200

    push for deploy

--- final_project/shop_bot/main.py
+++ final_project/shop_bot/main.py
@@ -1,16 +1,22 @@
-from telebot import TeleBot
+from bot import TGBot
 from config import TOKEN
 from keyboards import START_KB
-from models.model import Category, Product, Texts
+from models.model import Category, Product, Cart
 
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-bot = TeleBot(token=TOKEN)
+from telebot.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+bot = TGBot(token=TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    txt = Texts.objects.filter(
-        text_type='Greetings'
-    ).get()
+    # txt = Texts.objects.filter(
+    #     text_type='Greetings'
+    # ).get()
+    txt = 'hello'
 
     kb = ReplyKeyboardMarkup()
     buttons = [KeyboardButton(button_name) for button_name in START_KB.values()]
@@ -20,3 +26,71 @@ def start(message):
     bot.send_message(message.chat.id,
                      txt,
                      reply_markup=kb)
+
+@bot.message_handler(func=lambda message: message.text == START_KB['categories'])
+def categories(message):
+    cats = Category.objects.filter(is_root=True)
+
+    kb = InlineKeyboardMarkup()
+
+    buttons = [
+        InlineKeyboardButton(text=cat.title, callback_data=str(cat.id)) for cat in cats
+    ]
+
+    kb.add(*buttons)
+    bot.send_message(message.chat.id, text='Выберите категорию', reply_markup=kb)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'category')
+def get_cat_or_products(call):
+    """
+    Приходит к нам id категории, получаем объект этой категории:
+    1) Если объект не имеет предков - выводим продукты
+    2) Если объект имеет предков выводим этих предков
+    :param call:
+    :return:
+    """
+
+    kb = InlineKeyboardMarkup()
+
+    category = Category.objects.get(id=call.data.split('_'[1]))
+
+    if category.subcategories:
+        buttons = [
+            InlineKeyboardButton(text=cat.title, callback_data='category_' + str(cat.id)) for cat in category.subcategories
+        ]
+    else:
+        buttons = [
+            InlineKeyboardButton(text=product.title, callback_data='product_' + str(product.id))
+            for product in category.get_products()
+        ]
+
+
+
+    kb.add(*buttons)
+    bot.edit_message_text(category.title, message_id=call.message.message_id,
+                          chat_id=call.message.chat.id, reply_markup=kb)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'product')
+def add_to_cart(call):
+    cart = Cart.get_or_create_cart(user_id=call.message.chat.id)
+    cart.add_product_to_cart(product_id=call.data.split('_')[1])
+    bot.answer_callback_query()
+
+
+
+
+"""
+WEBHOOK_HOST = https://33.46.32.19:8443
+PKEM = '/home/certificates/webhok_pkem.pem'
+PKEY = '/home/certificates/webhook_pkey.pem'
+bot.set_webhhook(WEBHOOK_HOST, open('r', PKEM))
+"""
+
+
+bot.set_webhook()
+bot.polling()
+
+
+
--- final_project/shop_bot/models/model.py
+++ final_project/shop_bot/models/model.py
@@ -1,12 +1,18 @@
 from mongoengine import *
-connect('shop')
+connect('shop2')
 
 
 class User(Document):
-    telegram_id = StringField(max_length=32, required=True)
+
+    STATES = (
+        ('products', 'products'),
+        ('categories', 'categories')
+    )
+    telegram_id = StringField(max_length=32, required=True, unique=True)
     username = StringField(max_length=128)
     fullname = StringField(max_length=256)
     phone_number = StringField(max_length=20)
+    state = StringField(choices=STATES)
     email = EmailField()
 
 
@@ -14,8 +20,38 @@ class Cart(Document):
     user = ReferenceField(User)
     is_archived = BooleanField(default=False)
 
-    def get_cart(self):
+    @classmethod
+    def get_or_create_cart(cls, user_id):
+        user = User.objects.get(id=user_id)
+        cart = cls.objects.get(user=user, is_archived=False)
+
+        if not cart:
+            cart = cls.objects.create(user=user)
+        return cart
+
+
+    def get_cart_products(self):
         return CartProduct.objects.filter(cart=self)
+
+    def add_product_to_cart(self, product_id):
+
+        # list_of_product_for_insert = [
+        #     CartProduct(cart=self,
+        #                 product=product) for product in products
+        # ]
+        #
+        # CartProduct.objects.insert(*list_of_product_for_insert)
+        CartProduct.objects.create(
+            cart=self,
+            product=Product.get_product(id=product_id)
+        )
+
+    def delete_product_from_cart(self, product):
+        CartProduct.objects.filter(
+            cart=self,
+            product=product
+        ).first().delete()
+
 
     # TO DO
     # Overthink
@@ -36,7 +72,7 @@ class Attributes(EmbeddedDocument):
 
 
 class Category(Document):
-    title = StringField(min_length=1, max_length=255, required=True)
+    title = StringField(min_length=1, max_length=255, required=True, unique=False)
     subcategories = ListField(ReferenceField('self'))
     parent = ReferenceField('self')
     is_root = BooleanField(default=False)
@@ -58,15 +94,15 @@ class Category(Document):
         if kwargs.get('parent') == True:
             kwargs['is_root'] = False
 
-        Product(**kwargs).save()
-
-
-
+        return cls(**kwargs).save()
 
     def get_products(self):
         Product.objects.filter(
             category=self
         )
+
+    def __str__(self):
+        return self.title
 
 
 class Product(Document):
@@ -80,6 +116,10 @@ class Product(Document):
     extra_data = StringField(max_length=4096)
     category = ReferenceField(Category, required=True)
 
+    @classmethod
+    def get_product(cls, **kwargs):
+        cls.objects.get(**kwargs)
+
     def get_price(self):
         return self.price if not self.discount_price else self.discount_price
 
@@ -92,6 +132,69 @@ class Texts(Document):
 
     text_type = StringField(choices=TEXT_TYPES)
     body = StringField(max_length=2048)
+
+
+
+if __name__ == "__main__":
+
+
+    #### Creation ####
+    # category_dict = {
+    #     'title': 'category1',
+    #     'description': 'category1 descritpion',
+    #     'is_root': True,
+    # }
+    # root_cat = Category.create(**category_dict)
+    #
+    # for i in range(5):
+    #     category_dict = {
+    #         'title': f'category{i}',
+    #         'description': f'category{i} descritpion',
+    #     }
+    #     sub_cat = Category(**category_dict)
+    #     root_cat.add_subcategory(sub_cat)
+    #### END ####
+
+    # cats = Category.objects.filter(is_root=True)
+    #
+    # for cat in cats:
+    #     print(cat)
+    #
+    #     if cat.subcategories:
+    #         for sub in cat.subcategories:
+    #             print(f'Parent is {sub.parent}')
+    #             print(f'sub cat - {sub}')
+
+
+
+    #ITEMS FREQUENCIES
+    # user = User.objects.create(telegram_id='12345')
+    #
+    # cart = Cart.objects.create(user=user)
+
+
+    cart = Cart.objects.first()
+
+    product = Product.objects.first()
+    cart.add_product_to_cart(product)
+    frequencies = cart.get_cart().item_frequencies('product')
+    # for i in range(10):
+    #
+    #     prod = {
+    #         'title': f'title {i}',
+    #         'article': f'article {i}',
+    #         'category': Category.objects.first(),
+    #         'price': 10 * i + 1
+    #     }
+    #     created_product = Product.objects.create(
+    #         **prod
+    #     )
+    #     cart.add_product_to_cart(created_product)
+
+
+
+
+
 
 
 
